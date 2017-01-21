e9936e183f06c0bd760d85bddea4ffca580c0674
Rahul Kant Ravi <rahulkantcs@gmail.com>
2017-01-21 14:09:13 +0530

    using factory pattorn

---
+++ js/MenuController.js
@@ -0,0 +1,18 @@
+function MenuControllerFactory($scope){
+   return function ($scope) {
+
+        $scope.todaysMenu = '';
+        $scope.message = '';
+        $scope.tasteMenu = function () {
+            var menuList = $scope.todaysMenu.split(',');
+            if (menuList == '') {
+                $scope.message='Please Enter The Menu';
+            }else if (menuList.length <= 3) {
+                $scope.message='Enjoy';
+            }else{
+                $scope.message='Too Much!!';
+            }
+        }
+
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,24 +1,7 @@
 (function () {
 
     var app = angular.module('LunchMenu', []);
-    app.controller('MenuController', MenuController);
-    MenuController.$inject = ['$scope'];
-
-    function MenuController($scope) {
-
-        $scope.todaysMenu = '';
-        $scope.message = '';
-        $scope.tasteMenu = function () {
-            var menuList = $scope.todaysMenu.split(',');
-            if (menuList == '') {
-                $scope.message='Please Enter The Menu';
-            }else if (menuList.length <= 3) {
-                $scope.message='Enjoy';
-            }else{
-                $scope.message='Too Much!!';
-            }
-        }
-
-    }
+    app.controller('MenuController', MenuControllerFactory());
+    MenuControllerFactory.$inject = ['$scope'];
 
 })();
